f967aa58c080507e7eba21a0176c4ea6613054cd
Toshiaki <toshi-h.0484@keio.jp>
2021-10-11 19:11:04 +0900

    log出力機能の追加

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import sys
 import pandas as pd
 import random as rd
 import string
+import datetime as dt
 
 if __name__ == "__main__":
     #ipf = str(input("入力ファイルのPATHを入力してください。\n>>"))
@@ -13,31 +14,45 @@ if __name__ == "__main__":
     #dup = bool(input("重複当選の有無を入力してください。(True/False)\n>>"))
     dup = False
 
-    ipf = pd.read_csv(ipf, encoding="cp932")
-    print(ipf)
+    with open("./choice.log", mode="a", encoding="utf-8_sig") as log:
+        now = dt.datetime.now()
+        log.write("-----" + str(now) + "の実行ログ-----\n")
+        ipf = pd.read_csv(ipf, encoding="cp932")
+        print(ipf)
 
-    dice = list(range(0, len(ipf)))
-    weight = list(ipf["weight"])
+        dice = list(range(0, len(ipf)))
+        weight = list(ipf["weight"])
 
-    ipf["result"] = ""
-    if dup:
-        for chance in range(num):
-            choice = rd.choices(dice, weights=weight)
-            print(choice)[0]
-            ipf.loc[choice, "result"] += list(string.ascii_lowercase)[chance]
-    else:
-        if not((len(ipf) - num) > -1):
-            print("候補数が少ない、あるいは当選数が多すぎます。再設定してください。")
-            sys.exit()
-        for chance in range(num):
-            while True:
-                choice = rd.choices(dice, weights=weight)[0]
-                if ipf.loc[choice, "result"] == "":
-                    ipf.loc[choice, "result"] = list(
-                        string.ascii_lowercase)[chance]
-                    break
-                else:
-                    continue
+        ipf["result"] = ""
+        if dup:
+            for chance in range(num):
+                choice = rd.choices(dice, weights=weight)
+                print(choice)[0]
+                ipf.loc[choice,
+                        "result"] += list(string.ascii_lowercase)[chance]
+                print(">>", ipf.loc[choice, "choice"], "さんが当選しました。")
+                log.write(">>" + ipf.loc[choice, "choice"] + "さんが当選しました。\n")
+        else:
+            if not((len(ipf) - num) > -1):
+                print("候補数が少ない、あるいは当選数が多すぎます。再設定してください。")
+                log.write("候補数が少ない、あるいは当選数が多すぎます。再設定してください。\n")
+                sys.exit()
+            for chance in range(num):
+                while True:
+                    choice = rd.choices(dice, weights=weight)[0]
+                    if ipf.loc[choice, "result"] == "":
+                        ipf.loc[choice, "result"] = list(
+                            string.ascii_lowercase)[chance]
+                        print(">>", ipf.loc[choice, "choice"], "さんが当選しました。")
+                        log.write(
+                            ">>" + ipf.loc[choice, "choice"] + "さんが当選しました。\n")
+                        break
+                    else:
+                        print(">>", ipf.loc[choice, "choice"],
+                              "さんが当選しましたが、重複したため再抽選します。")
+                        log.write(
+                            ">>" + ipf.loc[choice, "choice"] + "さんが当選しましたが、重複したため再抽選します。\n")
+                        continue
 
-    print(ipf)
-    ipf.to_csv("result.csv", encoding="utf-8_sig")
+        print(ipf)
+        ipf.to_csv("result.csv", encoding="utf-8_sig")
